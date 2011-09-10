44056896648509f1e801c8e496439346787e2513
Rico Sta. Cruz <rico@ambiescent.com>
2011-09-10 10:24:51 +0800

    .

--- app/init/templates.rb
+++ app/init/templates.rb
@@ -2,7 +2,7 @@ Encoding.default_external = 'utf-8'
 
 class Main
   set :scss, load_paths: [ root('app/css') ]
-  set :scss, scss.merge(line_numbers: true, debug_info: true, always_check: true) if development?
+  set :scss, scss.merge(line_numbers: true, always_check: true) if development?
   set :scss, scss.merge(style: :compressed) if production?
   set :sass, scss
   set :haml, escape_html: true, layout: :layout, ugly: true
