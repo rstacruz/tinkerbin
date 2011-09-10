b7d48c5af80476f45526ce9e2ee5ad2f1a9307ca
Rico Sta. Cruz <rico@ambiescent.com>
2011-09-10 13:46:25 +0800

    Fix Heroku.

--- config/defaults/sequel.rb
+++ config/defaults/sequel.rb
@@ -1,10 +1,10 @@
 require Main.root('app/init/sequel.rb')
 require 'fileutils'
 
-FileUtils.mkdir_p 'db'
-
 if Main.test?
+  FileUtils.mkdir_p 'db'
   Main.database_url = "sqlite://db/test.db"
 else
+  FileUtils.mkdir_p 'db'
   Main.database_url = ENV['DATABASE_URL'] || "sqlite://db/development.db"
 end
